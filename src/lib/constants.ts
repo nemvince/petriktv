fead23ac512c8ba4c73d7d880dd8774797f25fcc
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-01-23 11:24:36 +0100

    feat: faster Sub and Bus fetching, sorting consolidated subs to end of starting lesson

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,8 +1,8 @@
 import { Departure } from '@/schema/types';
 
 export const REFETCH_INTERVALS = {
-	substitutions: 60 * 1000,
-	roomSubtitutions: 2 * 60 * 1000,
+	substitutions: 30 * 1000,
+	roomSubtitutions: 60 * 1000,
 	news: 2 * 60 * 1000,
 	busDepartures: 30 * 1000,
 	weather: 10 * 60 * 1000,
