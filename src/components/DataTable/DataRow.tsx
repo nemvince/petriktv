aba2087a14a32a6a6b1efd15f65886ae9ed645d3
hrustinszkiadam <hrustinszkiadam@gmail.com>
2025-01-25 17:51:22 +0100

    feat: row height calculation

--- src/components/DataTable/DataRow.tsx
+++ src/components/DataTable/DataRow.tsx
@@ -1,13 +1,22 @@
+import { ITEMS_PER_PAGE } from '@/lib/constants';
 import { RoomSubstitutionEntry, Substitution } from '@/schema/types';
+import { useMemo } from 'react';
 
 type DataRowProps = {
 	data: Substitution | RoomSubstitutionEntry;
 	rowNumber: number;
 };
 const DataRow = ({ rowNumber }: DataRowProps) => {
+	const height = useMemo(() => {
+		return `h-1/${ITEMS_PER_PAGE} max-h-1/${ITEMS_PER_PAGE}`;
+	}, [ITEMS_PER_PAGE]);
+	const bgColor = useMemo(() => {
+		return rowNumber % 2 == 0 ? 'bg-opacity-20' : 'bg-opacity-0';
+	}, [rowNumber]);
+
 	return (
 		<div
-			className={`border-b border-petrik-3 bg-black last:border-opacity-80 ${rowNumber % 2 == 0 ? 'bg-opacity-20' : 'bg-opacity-0'}`}
+			className={`flex items-center justify-between border-b border-petrik-3 bg-black last:border-opacity-80 ${bgColor} ${height}`}
 		>
 			DataRow
 		</div>
